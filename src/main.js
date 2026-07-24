f59a37bcf15bd3bdbc1989914b7c9780b2499220
haudemTNG <marie.haude@tngtech.com>
2026-07-24 13:51:17 +0200

    added expectations

--- src/main.js
+++ src/main.js
@@ -13,6 +13,6 @@ new App({
     // Which CI/CD tools do you use in your project?
     tools: "Jenkins",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["just reducing unknowns"],
   },
 });
