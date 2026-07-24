f848d7e87b646d6cb983c244dab8b9a3e9448b72
haudemTNG <marie.haude@tngtech.com>
2026-07-24 14:09:25 +0200

    text

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ new App({
     // What do you associate with the term 'CI/CD'?
     associations: ["red tests ^^"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins",
+    tools: "Jenkins?",
     // What do you want to learn in this workshop?
     expectations: ["just reducing unknowns"],
   },
